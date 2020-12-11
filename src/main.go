a776b7ea47398729775ee85c7096fa608a9f6c7f
bkds <bkdsjp@gmail.com>
2020-12-11 23:44:22 +0900

    コマンドラインからURLと出力先を指定できるようにした

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/url"
 	"os"
@@ -9,16 +10,14 @@ import (
 	"gownloder/src/downloder/idownloader"
 )
 
-const URL = "http://i.imgur.com/z4d4kWk.jpg"
-
 func main() {
-	// dummy
-	url, _ := url.ParseRequestURI(URL)
-	dst := "./out" + url.Path
+	u, d := parseFlag()
 
 	var iDownloader idownloader.IDownloader
 	iDownloader = downloder.NewDownloader()
 
+	url, _ := url.ParseRequestURI(u)
+	dst := fmt.Sprintf("%s%s", d, url.Path)
 	if err := iDownloader.DownloadFile(*url, dst); err != nil {
 		fmt.Printf("Download Failed: %+v", err)
 
@@ -26,4 +25,25 @@ func main() {
 	}
 
 	fmt.Println("Download Successfully!")
+}
+
+func parseFlag() (url string, dst string) {
+	var (
+		u = flag.String("u", "", "URL of download file.")
+		d = flag.String("d", "", "Destination of downloaded file.")
+	)
+
+	flag.Parse()
+
+	if *u == "" {
+		fmt.Println("Flag -u can not be null")
+		os.Exit(1)
+	}
+
+	if *d == "" {
+		fmt.Printf("Flag -d can not be null ")
+		os.Exit(1)
+	}
+
+	return *u, *d
 }
